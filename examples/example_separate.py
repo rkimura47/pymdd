ec2b6e8abe0a7e8bc3e55c7e04cd200af943c623
Ryo Kimura <rkimura47@gmail.com>
2019-02-04 15:37:37 -0500

    Modified example_separate.py to use hackish (but simpler) counter

--- examples/example_separate.py
+++ examples/example_separate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from pymdd.mdd import *
 from os import system
-from uuid import uuid1
 
 # Compilation by Separation Example from Section 3.11 of Bergman et. al, Decision Diagrams for Optimization
 G = [[(1,2), (1,3)], [(2,1), (2,3)], [(3,1), (3,2)]]
@@ -24,13 +23,20 @@ isFeas = lambda s,j: s is not None
 maxWidth = lambda j: 100
 name = 'smisp'
 
+def counter(k = [-1]):
+    k[0] += 1
+    return k[0]
+# This is admittedly a bit of a hack... a more 'appropriate' option would be
+#from uuid import uuid1
+#counter = uuid1
+
 
 
 # Construct the MDD
 mymdd = MDD(name)
 
-mymdd.compile_trivial(numLayers, domain, lambda d,j: costFunc(None,d,j,None), lambda j: uuid1())
-mymdd.filter_and_refine_constraint(trFunc, rootState, isFeas, lambda s,j: uuid1(), maxWidth)
+mymdd.compile_trivial(numLayers, domain, lambda d,j: costFunc(None,d,j,None), lambda j: counter())
+mymdd.filter_and_refine_constraint(trFunc, rootState, isFeas, lambda s,j: counter(), maxWidth)
 mymdd.reduce_bottom_up(lambda slist,j: slist[0])
 
 # Print the contents
